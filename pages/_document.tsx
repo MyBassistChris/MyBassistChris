27e9964da33be83f8b3db26c5a197b0cc28d7bfc
MyBassistChris <52180588+MyBassistChris@users.noreply.github.com>
2020-06-09 16:20:00 -0700

    Analytics fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import Menu from "../components/Menu/Menu"
 import Footer from "../components/Footer/Footer"
-import ReactGA from 'react-ga'
+
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,12 +11,27 @@ class MyDocument extends Document {
   }
 
   render() {
-    ReactGA.initialize('UA-155083063-1')
-    ReactGA.pageview(window.location.pathname + window.location.search)
     return (
       <Html>
         <Head>
           <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+          <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', '${GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                    });
+                  `,
+                }}
+              />
         </Head>
         <body>
           <Menu />
